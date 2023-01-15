300d9159511505d22fb5a2dbd6c75b90df353621
Vanessa Vallarini <vanessa.vallarini@ifood.com.br>
2023-01-15 18:49:41 -0300

    feat: docs

--- docs/diagrams/src/architecture/account_consumer_service_-_architecture_diagram.py
+++ docs/diagrams/src/architecture/account_consumer_service_-_architecture_diagram.py
@@ -1,18 +1,22 @@
 import os, sys
+from urllib.request import urlretrieve
 
 os.chdir(os.path.dirname(sys.argv[0]))
 
 from diagrams import Cluster, Diagram, Edge
 from diagrams.k8s.compute import Pod
 from diagrams.onprem.queue import Kafka
-from diagrams.aws.database import Aurora
+from diagrams.custom import Custom
+from diagrams.programming.language import Go
 from diagrams.k8s.network import Service
 
-
-with Diagram("account producer service", show = False):
+with Diagram("account producer service", show = False, direction="TB"):
     blueline=Edge(color="blue",style="bold")
     darkOrange=Edge(color="darkOrange",style="bold")
     blackline=Edge(color="black",style="bold")
+    scylladb_url = "https://upload.wikimedia.org/wikipedia/en/2/20/Scylla_the_sea_monster.png"
+    scylladb_icon = "scylla.png"
+    urlretrieve(scylladb_url, scylladb_icon)
 
     with Cluster("account-producer-pod"):
         producerPod=Pod("account-producer-pod")
@@ -20,11 +24,13 @@ with Diagram("account producer service", show = False):
     with Cluster("external"):
        producerCreateKafka=Kafka("account-create")
        producerUpdateKafka=Kafka("account-update") 
-       accountAvros=Service("account-toolkit")  
        producerViaCepApi=Service("via-cep-api")
 
+    with Cluster("internal"):
+       accountAvros=Go("account-toolkit")
+
     with Cluster("scyllaDb"):
-       accountDatabase=Aurora("account-database")
+       accountDatabase=Custom("account-database",scylladb_icon)
 
     producerPod - darkOrange >> producerCreateKafka
     producerPod - darkOrange >> producerUpdateKafka
